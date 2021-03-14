84ab36c9e9a7baba8078cf41f32510ce1c83e952
kpj <kim.philipp.jablonski@gmail.com>
2021-03-14 18:58:56 +0100

    Set log-level of uvicorn to 'warning'

--- plyder/main.py
+++ plyder/main.py
@@ -7,7 +7,7 @@ def main():
     config['download_directory'].mkdir(parents=True, exist_ok=True)
 
     uvicorn.run(
-        'plyder.app:app', host='0.0.0.0', port=5000, log_level='info', reload=False
+        'plyder.app:app', host='0.0.0.0', port=5000, log_level='warning', reload=False
     )
 
 
